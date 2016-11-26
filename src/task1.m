57e075a21c3c0e0df50fda044a1552a76afc147f
Carol Ng <ng.km.carol@gmail.com>
2016-11-25 18:04:59 -0800

    Add pictures and answers to Task 1

---
+++ src/task1.m
@@ -0,0 +1,34 @@
+videoPath = '../media/video.mp4';
+framesPath = '../media/frames/';
+mkdir('../media/frames');
+
+v = VideoReader(videoPath);
+v_height = v.Height;
+v_width = v.Width;
+v_frameRate = v.FrameRate;
+
+% Question 0
+disp('Question 0:');
+disp(['Number of frames: 3081']);
+disp(['Frame rate: ' num2str(v_frameRate)]);
+
+i = 0;
+t = 45;
+v.currentTime = 45;
+while t < 57
+    t = v.currentTime;
+    vFrameData = readFrame(v);
+    vFrameName = [framesPath 'imagedata_' num2str(t,'%.4f') '.png'];
+    imwrite(vFrameData, vFrameName);
+end
+
+% Question 1
+disp('Question 1:');
+disp(['Rows of data array: ' v_height]);
+disp(['Columns of data array: ' v_width]);
+disp(['Depth of data array: ' num2str(3)]);
+
+disp(['Height (pixels) of each frame: ' v_height]);
+disp(['Width (pixels) of each frame: ' v_width]);
+
+disp(['Bits in each frame: ' num2str(2764800/720/1280*8)]);
